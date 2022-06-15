73fb50b7bada593a8ef1e30bccfe8f58483f3595
viettrungIT3 <viettrungcntt03@gmail.com>
2022-06-15 22:56:08 +0700

    updated feature - management users by admin

--- classes/user.php
+++ classes/user.php
@@ -96,6 +96,40 @@ class user
 		}
 		return false;
 	}
+	public function getAllAdmin($page = 1, $total = 8)
+    {
+        if ($page <= 0) {
+            $page = 1;
+        }
+        $tmp = ($page - 1) * $total;
+        $query =
+            "SELECT users.*, role.name as roleName
+			 FROM users INNER JOIN role ON users.role_id = role.id
+             limit $tmp,$total";
+        $result = $this->db->select($query);
+        return $result;
+    }
+
+    public function getAll()
+    {
+        $query =
+            "SELECT users.*, role.name as cateName
+			 FROM users INNER JOIN role ON users.role_id = role.id";
+        $result = $this->db->select($query);
+        return $result;
+    }
+
+	public function getCountPaging($row = 8)
+    {
+        $query = "SELECT COUNT(*) FROM users";
+        $mysqli_result = $this->db->select($query);
+        if ($mysqli_result) {
+            $totalrow = intval((mysqli_fetch_all($mysqli_result, MYSQLI_ASSOC)[0])['COUNT(*)']);
+            $result = ceil($totalrow / $row);
+            return $result;
+        }
+        return false;
+    }
 
 	public function getLastUserId()
 	{
@@ -122,5 +156,37 @@ class user
 		}
 		return 'Mã xác minh không đúng!';
 	}
+
+	public function block($id)
+    {
+        $query = "UPDATE users SET status = 0 where id = '$id' and role_id = 2 ";
+        $result = $this->db->delete($query);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($id)
+    {
+        $query = "DELETE FROM users WHERE id = $id";
+        $row = $this->db->delete($query);
+        if ($row) {
+            return true;
+        }
+        return false;
+    }
+
+    public function active($id)
+    {
+        $query = "UPDATE users SET status = 1 where id = '$id'  and role_id = 2 ";
+        $result = $this->db->delete($query);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 ?>
